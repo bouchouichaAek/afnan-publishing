40b26179bb1a63bab6a2a7549161f508db860183
Bouchouicha Abdelkader <bouchouichaaek97@gmail.com>
2023-08-13 11:00:25 +0100

    add landing and release books section

---
+++ js/alqabas.js
@@ -0,0 +1,62 @@
+var swiper0 = new Swiper(".swiper0", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: false,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination0",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next0",
+    prevEl: ".swiper-button-prev0",
+  },
+});
+
+var swiper1 = new Swiper(".swiper1", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  autoplay: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+});
+
+var swiper1 = new Swiper(".swiper2", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  autoplay: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+});
